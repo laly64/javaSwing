8966552d3a2a83030fa4d42c8b0e607208c65172
laly64 <avalos53laly@gmail.com>
2017-11-08 21:52:11 -0300

    Se han hecho los pasos del 1 al 7

--- src/Swing1/ventana00.java
+++ src/Swing1/ventana00.java
@@ -3,12 +3,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package Swing1;
 
+import javax.swing.*; 
+import java.awt.*;
 /**
  *
  * @author LAB02
  */
-public class ventana00 {
+
+
+public class ventana00 extends JFrame 
+  
+{
+    JPanel mipanel; 
+    JLabel milabel;
+    JTextField mitexto;
+    JButton miboton;
+    JRadioButton radio;
+    JCheckBox caja;
     
+    public ventana00() 
+    {       
+      mipanel = new JPanel();
+      milabel = new JLabel();
+      mitexto = new JTextField(10);
+      miboton = new JButton();  
+      radio = new JRadioButton();
+       caja = new JCheckBox();
+       
+        this.add(mipanel); 
+      mipanel.add(milabel);
+      mipanel.add(mitexto);
+      mipanel.add(miboton);
+      mipanel.add(caja);
+      mipanel.add(radio);
+      
+      
+     milabel.setText("nombre");
+     miboton.setText("Enviar");
+     mipanel.setBackground(Color.green);
+     
+         
+     }
 }
