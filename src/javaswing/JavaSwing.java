d136519d7f75da793fe0fbc12a9c1189c95c89af
laly64 <avalos53laly@gmail.com>
2017-11-08 21:53:18 -0300

    PASO 8, IMPORTAR LA CLASE

---
+++ src/javaswing/JavaSwing.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaswing;
+import javax.swing.*; 
+import java.awt.*;
+
+import Swing1.ventana00;
+
+public class JavaSwing 
+{ 
+
+/**
+ *
+ * @author LAB02
+ */
+
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public javaSwing() 
+    {
+        // TODO code application logic here
+    }
+    
+}
